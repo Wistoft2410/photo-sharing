118007f5aadf20078bdf106698e157405a891f87
Adam Isaac <44964494+Wistoft2410@users.noreply.github.com>
2024-09-02 14:42:52 +0200

    Update Album.js
    Added scrollview and displaying title components inside

--- photo-sharing/src/components/Albums.js
+++ photo-sharing/src/components/Albums.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import {View, Text, Pressable} from 'react-native';
+import { View, Text, ScrollView, StyleSheet, Pressable } from 'react-native'; // Make sure StyleSheet is included
+import theme from '../../assets/themes/index';
+import albumPage from '../../assets/data/albumPage';
 
-const Albums = ({ navigation }) => {
+const Albums = ({ i }) => {
     return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>Albums page!</Text>
-            <Pressable onPress={() => navigation.navigate("Shared Album")}>
-                <Text>Click me</Text>
-            </Pressable>
-        </View>
+        <>
+        <ScrollView>
+            <View style={styles.albumContainer}> 
+                {albumPage.map((item, index) => 
+                <View key={index}> 
+                    <Text>{item.title}</Text>
+                </View>
+                )}
+            </View>
+        </ScrollView>
+        </>
     );
 };
+
+const styles = StyleSheet.create({
+    albumContainer: {
+        marginBottom: theme.spacing.l,
+    }
+})
 
 export default Albums ;
