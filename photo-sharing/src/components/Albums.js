c521738699b6df206fed0af8056f644994288da2
Adam Isaac <44964494+Wistoft2410@users.noreply.github.com>
2024-09-09 02:37:12 +0200

    Added a seperator

--- photo-sharing/src/components/Albums.js
+++ photo-sharing/src/components/Albums.js
@@ -3,6 +3,7 @@ import { View, Text, ScrollView, StyleSheet, Pressable } from 'react-native'; //
 import theme from '../../assets/themes/index';
 import albumPage from '../../assets/data/albumPage';
 import Card from './Card';
+import Seperator from './Seperator';
 
 const Albums = ({ navigation }) => {
     return (
@@ -12,7 +13,11 @@ const Albums = ({ navigation }) => {
                 {albumPage.map((item, index) => 
                 <View key={index}> 
                     <Card item={item}/>
+                
+                {/* Add seperator if the index is 1 */}
+                {index === 1 && <Seperator />}
                 </View>
+
                 )}
             </View>
         </ScrollView>
